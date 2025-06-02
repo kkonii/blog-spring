7da6ed1fc7b1e9b36aace4f3884db1dce7b8c268
kkonii <119216472+kkonii@users.noreply.github.com>
2025-06-02 19:10:49 +0900

    feat: build filter chain for particular http request

--- src/main/java/com/blog/config/WebSecurityConfig.java
+++ src/main/java/com/blog/config/WebSecurityConfig.java
@@ -6,7 +6,10 @@ import com.blog.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
@@ -20,5 +23,26 @@ public class WebSecurityConfig {
         return (web) -> web.ignoring()
                 .requestMatchers(toH2Console())
                 .requestMatchers("/static/**");
+    }
+
+    // 특정 HTTP 요청에 대한 웹 기반 보안을 구성
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        HttpSecurity security = http.authorizeHttpRequests(auth ->
+                auth.requestMatchers("/login", "/signup", "/user")
+                        .permitAll()
+                        .anyRequest().authenticated()
+        );
+
+        //로그인 설정
+        security.formLogin(auth -> auth.loginPage("/login")
+                .defaultSuccessUrl("/api/articles"));
+
+        //로그아웃 설정
+        security.logout(auth -> auth.logoutSuccessUrl("/login")
+                .invalidateHttpSession(true));
+
+        //csrf 비활성화
+        return security.csrf(AbstractHttpConfigurer::disable).build();
     }
 }
