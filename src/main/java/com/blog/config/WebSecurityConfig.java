96f18b0f7356193f87099c626f837add9efce311
kkonii <119216472+kkonii@users.noreply.github.com>
2025-06-03 21:43:18 +0900

    refactor: rename service interface

--- src/main/java/com/blog/config/WebSecurityConfig.java
+++ src/main/java/com/blog/config/WebSecurityConfig.java
@@ -2,7 +2,7 @@ package com.blog.config;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
-import com.blog.user.service.UserService;
+import com.blog.user.service.UserDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +18,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
-    private final UserService userService;
+    private final UserDetailService userService;
 
     @Bean
     //security 기능 비활성화 시키기
@@ -56,7 +56,7 @@ public class WebSecurityConfig {
 
     @Bean
     public AuthenticationManager authenticationManager(
-            HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserService userDetailService)
+            HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService)
             throws Exception {
 
         AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
--- src/main/java/com/blog/user/service/UserDetailService.java
+++ src/main/java/com/blog/user/service/UserDetailService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserService implements UserDetailsService {
+public class UserDetailService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
