8a006b36af20aa5981a4fb16d79faf0711ef11ed
kkonii <119216472+kkonii@users.noreply.github.com>
2025-06-04 14:35:34 +0900

    refactor: add api controller

---
+++ src/main/java/com/blog/user/controller/UserApiController.java
@@ -0,0 +1,21 @@
+package com.blog.user.controller;
+
+import com.blog.user.controller.dto.request.AddUserRequest;
+import com.blog.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserApiController {
+
+    private final UserService userService;
+
+    @PostMapping("/api/user")
+    public String signUp(AddUserRequest request) {
+        userService.save(request);
+
+        return "redirect:/login";
+    }
+}
