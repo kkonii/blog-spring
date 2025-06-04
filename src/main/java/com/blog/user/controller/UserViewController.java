bd0d9f3d39d2fe7085fb140911760a07cfc24f96
kkonii <119216472+kkonii@users.noreply.github.com>
2025-06-04 15:25:59 +0900

    feat: map to sign-up view

--- src/main/java/com/blog/user/controller/UserViewController.java
+++ src/main/java/com/blog/user/controller/UserViewController.java
@@ -11,4 +11,10 @@ public class UserViewController {
         //api/login 경로로 접근시 해당 메서드가 login.html 을 반환
         return "login";
     }
+
+    @GetMapping("api/signup")
+    public String signup() {
+        //api/signup 경로로 접근시 해당 메서드가 signup.html 을 반환
+        return "signup";
+    }
 }
