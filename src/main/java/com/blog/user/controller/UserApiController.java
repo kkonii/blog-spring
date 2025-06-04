c53240e00750a332140b4b1d4c419d65d37dedd4
kkonii <119216472+kkonii@users.noreply.github.com>
2025-06-04 20:55:34 +0900

    feat: implement logout

--- src/main/java/com/blog/user/controller/UserApiController.java
+++ src/main/java/com/blog/user/controller/UserApiController.java
@@ -2,8 +2,13 @@ package com.blog.user.controller;
 
 import com.blog.user.controller.dto.request.AddUserRequest;
 import com.blog.user.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -15,6 +20,14 @@ public class UserApiController {
     @PostMapping("/api/user")
     public String signUp(AddUserRequest request) {
         userService.save(request);
+
+        return "redirect:/api/login";
+    }
+
+    @GetMapping("/api/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler()
+                .logout(request, response, SecurityContextHolder.getContext().getAuthentication());
 
         return "redirect:/api/login";
     }
