df9d4e3254a9d0cad4348e0496a4850426ef4e53
kkonii <119216472+kkonii@users.noreply.github.com>
2025-06-12 19:42:18 +0900

    refactor: use getter annotation only for issuer value

--- src/main/java/com/blog/config/jwt/JwtProperties.java
+++ src/main/java/com/blog/config/jwt/JwtProperties.java
@@ -9,12 +9,12 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Getter
 @Setter
 @Component
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
 
+    @Getter
     private String issuer;
     private String secretKey;
 
