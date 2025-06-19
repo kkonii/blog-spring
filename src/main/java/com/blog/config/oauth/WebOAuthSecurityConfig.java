40906df6eb84a752e96e789a9bbdb9ef4ac65a22
kkonii <119216472+kkonii@users.noreply.github.com>
2025-06-20 00:48:37 +0900

    feat: add custom filter validating token value

--- src/main/java/com/blog/config/oauth/WebOAuthSecurityConfig.java
+++ src/main/java/com/blog/config/oauth/WebOAuthSecurityConfig.java
@@ -2,16 +2,22 @@ package com.blog.config.oauth;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
+import com.blog.config.TokenAuthenticationFilter;
+import com.blog.config.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebOAuthSecurityConfig {
+
+    private final TokenProvider tokenProvider;
 
     @Bean
     public WebSecurityCustomizer configure() {
@@ -21,13 +27,22 @@ public class WebOAuthSecurityConfig {
                 .requestMatchers("/img/**", "/css/**", "/js/**");
     }
 
+    @Bean
     // application context 초기화시 진행되는 로직
-    public void filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // 토큰으로 인증을 하기 때문에 기존의 사용하던 폼로그인, 세션 비활성화
         http.csrf(configurer -> configurer.disable())
                 .httpBasic(configurer -> configurer.disable())
                 .formLogin(configurer -> configurer.disable())
                 .logout(configurer -> configurer.disable())
                 .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // 헤더를 확인할 커스텀 filter 추가
+        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() throws Exception {
+        return new TokenAuthenticationFilter(tokenProvider);
     }
 }
